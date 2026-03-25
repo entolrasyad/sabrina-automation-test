925c9bc1e9788da9ec9ed13e39ed99c5af1b4113
entolrasyad <entol.rasyad@it.sharingvision.com>
2026-03-25 16:51:36 +0700

    chore: bump version

--- push.bat
+++ push.bat
@@ -6,7 +6,7 @@ if errorlevel 1 ( echo Gagal bump version & pause & exit /b 1 )
 echo [2/3] Commit semua perubahan...
 git add .
 git commit -m "chore: bump version"
-if errorlevel 1 ( echo Tidak ada perubahan untuk di-commit & )
+if errorlevel 1 ( echo Tidak ada perubahan untuk di-commit )
 
 echo [3/3] Push ke origin master...
 git push origin master
